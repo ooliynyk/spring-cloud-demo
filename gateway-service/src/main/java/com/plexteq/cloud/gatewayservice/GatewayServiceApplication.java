91fe58616aaccfa3df8b0c80fa9b589215f68b39
Oleksandr Oliynyk <o.p.oliynyk@gmail.com>
2018-05-06 18:18:43 +0300

    Move eureka props to config repo

--- gateway-service/src/main/java/com/plexteq/cloud/gatewayservice/GatewayServiceApplication.java
+++ gateway-service/src/main/java/com/plexteq/cloud/gatewayservice/GatewayServiceApplication.java
@@ -11,7 +11,6 @@ import org.springframework.hateoas.Resources;
 import org.springframework.hateoas.config.EnableHypermediaSupport;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.*;
